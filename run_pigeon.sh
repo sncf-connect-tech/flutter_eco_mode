fb2048f5f49898fdde1d360385159326716084c5
adrien_body <adrien_body@connect-tech.sncf>
2024-05-10 10:51:38 +0200

    test(codecov): add codecov github runner

---
+++ run_pigeon.sh
@@ -0,0 +1,5 @@
+dart run pigeon \
+ --input pigeons/messages.dart \
+ --dart_out lib/messages.dart \
+ --kotlin_out android/src/main/kotlin/sncf/connect/tech/flutter_eco_mode/Messages.kt \
+ --kotlin_package "sncf.connect.tech.flutter_eco_mode"
